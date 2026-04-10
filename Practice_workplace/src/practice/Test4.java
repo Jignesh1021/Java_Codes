50f5a9f05cc8a4e7b56043aa4f5ecda92344f0e1
More Jignesh Satish <morejignesh67@gmail.com>
2026-04-10 23:37:53 +0530

    Added practice examples demonstrating private access modifier and encapsulation

---
+++ Practice_workplace/src/practice/Test4.java
@@ -0,0 +1,15 @@
+package practice;
+
+class A {
+    private int x = 10;
+}
+
+public class Test4 {
+
+    public static void main(String[] args) {
+
+        A obj = new A();
+
+        // System.out.println(obj.x); error
+    }
+}
