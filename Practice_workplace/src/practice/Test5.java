d78972a78bf00f5124f883853348cf130550aff2
More Jignesh Satish <morejignesh67@gmail.com>
2026-04-10 23:38:48 +0530

    Added practice examples demonstrating private access modifier and encapsulation

---
+++ Practice_workplace/src/practice/Test5.java
@@ -0,0 +1,25 @@
+package practice;
+
+class BankAccount {
+
+    private double balance;
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
+
+public class Test5 {
+
+    public static void main(String[] args) {
+
+        BankAccount acc = new BankAccount();
+
+        acc.deposit(5000);
+        System.out.println("Balance: " + acc.getBalance());
+    }
+}
