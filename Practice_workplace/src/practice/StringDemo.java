ce3fb271db517af7cbbceef106203140465ea4ae
More Jignesh Satish <morejignesh67@gmail.com>
2026-04-12 22:36:24 +0530

    Added practice programs for string

---
+++ Practice_workplace/src/practice/StringDemo.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class StringDemo {
+
+    public static void main(String[] args) {
+
+        String s = "Java Programming";
+
+        System.out.println(s.length()); // length
+        System.out.println(s.toUpperCase()); // uppercase
+        System.out.println(s.toLowerCase()); // lowercase
+        System.out.println(s.substring(5)); // substring
+        System.out.println(s.contains("Java"));// check word
+    }
+}
