f5cbf3668ed1302bbeeac39a5fd5e17f77b4bb1a
More Jignesh Satish <morejignesh67@gmail.com>
2026-04-14 23:53:07 +0530

    Added Second Largest Number Code

---
+++ Practice_workplace/src/practice/SecondLargest.java
@@ -0,0 +1,23 @@
+package practice;
+
+public class SecondLargest {
+
+    public static void main(String[] args) {
+
+        int arr[] = { 10, 20, 5, 40, 25 };
+
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > first) {
+                second = first;
+                first = num;
+            } else if (num > second && num != first) {
+                second = num;
+            }
+        }
+
+        System.out.println("Second Largest: " + second);
+    }
+}
