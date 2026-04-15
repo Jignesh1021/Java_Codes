af027f904bb20c18cd612245904cd6e3a7d07aec
More Jignesh Satish <morejignesh67@gmail.com>
2026-04-15 23:52:26 +0530

    Added Missing Number Code

---
+++ Practice_workplace/src/practice/MissingNumber.java
@@ -0,0 +1,19 @@
+package practice;
+
+public class MissingNumber {
+
+    public static void main(String[] args) {
+
+        int arr[] = { 1, 2, 4, 5 };
+        int n = 5;
+
+        int total = n * (n + 1) / 2;
+        int sum = 0;
+
+        for (int num : arr) {
+            sum += num;
+        }
+
+        System.out.println("Missing: " + (total - sum));
+    }
+}
